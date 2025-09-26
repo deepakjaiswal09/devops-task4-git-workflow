326ddc2d52127796596e28145774c881640d177a
deepakjwll <deepakjaiswal9238@gmail.com>
2025-09-26 20:33:24 +0530

    push the files

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+def main():
+    print("Hello, DevOps Internship - Task 4!")
+    print("This is a sample Python app to demonstrate Git workflow.")
+
+if __name__ == "__main__":
+    main()
